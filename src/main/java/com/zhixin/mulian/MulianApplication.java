4f41f3831ded8f73edcb17e691e5828968be15d6
731166941@qq.com <Administrator@shibowen>
2020-05-28 14:55:54 +0800

    first

---
+++ src/main/java/com/zhixin/mulian/MulianApplication.java
@@ -0,0 +1,31 @@
+package com.zhixin.mulian;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class MulianApplication {
+
+	 public static void main(String[] args) throws UnknownHostException {
+	        ConfigurableApplicationContext application = SpringApplication.run(MulianApplication.class, args);
+	        Environment env = application.getEnvironment();
+	        String ip = InetAddress.getLocalHost().getHostAddress();
+	        String port = env.getProperty("server.port");
+	        String path = env.getProperty("server.servlet.context-path");
+	        log.info("\n----------------------------------------------------------\n\t" +
+	                "Application Happycycle-Boot is running! Access URLs:\n\t" +
+	                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+	                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+	                "----------------------------------------------------------");
+
+	    }
+
+}
